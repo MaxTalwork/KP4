9dea46cb1399fb28dc773d05a4d6ac25707b6a0c
Maxim Talancev <maxtalwork@gmail.com>
2024-05-11 03:26:23 +0300

    Добавил докстринги и комментарии

--- crs/class_hh.py
+++ crs/class_hh.py
@@ -3,17 +3,19 @@ from crs.class_parser import All
 
 
 class HH(All):
-
+    # класс для получения данных с HH
     def __init__(self):
         self.__url = 'https://api.hh.ru/vacancies'
 
     def get_information(self, text):
+        # метод класса для подключения к API
         params = {'name': text, 'area': 2, 'per_page': 100}
         response = requests.get(url=self.__url, params=params)
         return self.__conv_vac(response.json())
 
     @staticmethod
     def __conv_vac(data):
+        # Метод для преобразования полученной информации в нужный формат
         data_list = []
         for item in data['items']:
             items = {'text': (item['name']), 'url': (item['alternate_url']),
