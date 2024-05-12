3ca2540c8f466d0f233e7e886731fee103635001
Maxim Talancev <maxtalwork@gmail.com>
2024-05-12 20:34:41 +0300

    Исправили абстрактные классы. Добавили докстринги. Добавили валидацию зарплаты

--- crs/class_vacancy.py
+++ crs/class_vacancy.py
@@ -10,15 +10,32 @@ class Vacancy:
         self.url = url
         self.requirement = requirement
         self.responsibility = responsibility
-        self.salary = salary or 0
+        self.salary = salary
         self.salary_cor = currency
 
+    @property
+    def salary(self):
+        """
+        Проверка корректности введёной зарплаты
+        """
+        return self._salary
+
+    @salary.setter
+    def salary(self, value):
+        if value is None:
+            value = 0
+        self._salary = value
+
     def __lt__(self, other):
-        return self.salary < other.salary
+        return self._salary < other._salary
     """
     Функция сортировки для Класса Вакансий.
     """
 
     def __str__(self):
-        return (f'\nНазвание: {self.text}\nТребования: {self.requirement}\nОбязанности: {self.responsibility}\n'
-                f'Ссылка на вакансию: {self.url}\nЗарплата от: {self.salary} {self.salary_cor}')
+        if self._salary != 0:
+            return (f'\nНазвание: {self.text}\nТребования: {self.requirement}\nОбязанности: {self.responsibility}\n'
+                    f'Ссылка на вакансию: {self.url}\nЗарплата от: {self._salary} {self.salary_cor}')
+        else:
+            return (f'\nНазвание: {self.text}\nТребования: {self.requirement}\nОбязанности: {self.responsibility}\n'
+                    f'Ссылка на вакансию: {self.url}\nМинимальная зарплата не указана!')
